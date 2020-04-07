b00fb41aa9ef02b8cdfd24b16fa262aadb86bb47
Arkadiusz Grzywa <arrrusssdb@gmail.com>
2020-04-07 11:24:50 +0200

    register&login activity

---
+++ app/src/main/java/com/example/justcook/LoginActivity.java
@@ -0,0 +1,23 @@
+package com.example.justcook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        findViewById(R.id.backToRegister_editText_login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/justcook/MainActivity.java
+++ app/src/main/java/com/example/justcook/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.justcook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     int counter = 0;
@@ -15,9 +17,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText email = findViewById(R.id.email_editText_register);
-        EditText password = findViewById(R.id.password_editText_register);
-        Button register = findViewById(R.id.register_button_register);
+        EditText email = findViewById(R.id.email_editText_login);
+        EditText password = findViewById(R.id.password_editText_login);
+        Button register = findViewById(R.id.register_button_login);
 
         String testEmail = email.getText().toString();
         String testPassword = password.getText().toString();
@@ -31,6 +33,16 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 counter++;
                 Log.d("MainActivit","Button was clicked: "+counter + " times");
+            }
+        });
+
+        TextView alreadyHaveAccount = findViewById(R.id.alreadyHaveAccount_textView_register);
+        alreadyHaveAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("MainActivit","LogIn Activity");
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
             }
         });
     }
