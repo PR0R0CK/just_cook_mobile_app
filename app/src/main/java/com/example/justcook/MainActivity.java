a8e3e3183248fa8449a6f4680085681a65a231db
Arkadiusz Grzywa <arrrusssdb@gmail.com>
2020-04-16 12:20:12 +0200

    ideea

--- app/src/main/java/com/example/justcook/MainActivity.java
+++ app/src/main/java/com/example/justcook/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.justcook;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,32 +10,62 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-    int counter = 0;
+    private int counter = 0;
+    private EditText etEmail,etPassword;
+    private String email,password;
     private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String testEmail = this.<EditText>findViewById(R.id.email_editText_login).getText().toString();
-        String testPassword = this.<EditText>findViewById(R.id.password_editText_login).getText().toString();
+        etEmail =
+        email = this.<EditText>findViewById(R.id.email_editText_login).getText().toString();
+        password = this.<EditText>findViewById(R.id.password_editText_login).getText().toString();
 
-        Log.d("MainActivit","Email: "+testEmail);
-        Log.d("MainActivit","Email: "+testPassword);
+        Log.d("BEFMainActivitREGISTER", "Email: " + email);
+        Log.d("BEFMainActivitREGISTER", "Password: " + password);
 
         //Firebase Authentication
-//        FirebaseAuth.getInstance();
+        // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
+
+
 
         findViewById(R.id.register_button_login).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 counter++;
+                email = this.<EditText>findViewById(R.id.email_editText_login).getText().toString();
+                password = this.<EditText>findViewById(R.id.password_editText_login).getText().toString();
                 Log.d("MainActivit","Button was clicked: "+counter + " times");
+                Log.d("MainActivit", "Email: " + email);
+                Log.d("MainActivit", "Password: " + password);
+
+//                mAuth.createUserWithEmailAndPassword(email, password)
+//                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+//                            @Override
+//                            public void onComplete(@NonNull Task<AuthResult> task) {
+//                                if (task.isSuccessful()) {
+//
+//                                    Log.d("MAIN ACTIVITY", "createUserWithEmail:success");
+//                                    FirebaseUser user = mAuth.getCurrentUser();
+//                                } else {
+//                                    Log.d("MAIN ACTIVITY","failed !!!!!!!!!");
+//                                    Toast.makeText(MainActivity.this, "ERROR",Toast.LENGTH_LONG).show();
+//                                }
+//                            }
+//                        });
             }
         });
 
