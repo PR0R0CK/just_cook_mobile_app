7074f96d44587976e878d0179bae2c09d9d1e385
Lukasz Zaranek <l.zaranek01@gmail.com>
2020-06-05 14:47:34 +0200

    Otwieranie activity związanego z szczegółami przepisu

---
+++ app/src/main/java/com/example/justcook/activities/RecipeDetails.java
@@ -0,0 +1,16 @@
+package com.example.justcook.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.justcook.R;
+
+public class RecipeDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_details);
+    }
+}
