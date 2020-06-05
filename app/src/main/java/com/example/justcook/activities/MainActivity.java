6df10ada3b9810b49e7c5cb9bf9ff0751844a6ce
Arkadiusz Grzywa <arrrusssdb@gmail.com>
2020-06-05 21:44:59 +0200

    Sufit wygładzony... prawie jeszcze zostały ze dwie warstwy

--- app/src/main/java/com/example/justcook/activities/MainActivity.java
+++ app/src/main/java/com/example/justcook/activities/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.justcook.activities;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -14,27 +13,18 @@ import com.google.gson.GsonBuilder;
 import android.os.Bundle;
 import okhttp3.*;
 import android.util.Log;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.example.justcook.R;
 import com.example.justcook.model.RecipeBook;
 import com.google.android.material.navigation.NavigationView;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+
 import com.google.gson.reflect.TypeToken;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-import static java.sql.DriverManager.println;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     ArrayList<RecipeBook> recipeBooks = new ArrayList<>();
@@ -52,12 +42,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         toggle.syncState();
 
         getRecipes();
-        ArrayList<RecipeBook> test = new ArrayList<>();
-        //TODO: metoda getRecipes powinna zwracać ArrayList. Zamiana zmiennej test, na zwracaną listę sprawi, że recyclerView zapełni się wartościami z bazy
-        test.add(new RecipeBook("userId","recipeId","name","type","picture","ingredients","recipe","difficulty","rate"));
-        test.add(new RecipeBook("userId2","recipeId2","name2","type2","picture2","ingredients2","recipe2","difficulty2","rate2"));
-        initRecyclerView(test);
-
     }
 
     private void getRecipes() {
@@ -78,14 +62,17 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
                 Gson gson = new GsonBuilder().create();
 
-//                ArrayList<RecipeBook> recipeBooks = gson.fromJson(body,ArrayList<RecipeBook>);
-
                 Type listType = new TypeToken<ArrayList<RecipeBook>>(){}.getType();
                 recipeBooks = gson.fromJson(body, listType);
 
-//                List<RecipeBook> recipeBooks = new Gson().fromJson()
                 Log.d("ShowFood ",recipeBooks.get(0).toString());
                 Log.d("ShowFood ", String.valueOf(recipeBooks.size()));
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        initRecyclerView(recipeBooks);
+                    }
+                });
             }
         });
     }
