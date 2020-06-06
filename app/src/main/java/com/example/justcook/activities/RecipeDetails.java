23bbec819336cf73a9a29bcbeecf76e6ff470270
Lukasz Zaranek <l.zaranek01@gmail.com>
2020-06-06 16:03:02 +0200

    chce se spullować i musze walnąć kommita

--- app/src/main/java/com/example/justcook/activities/RecipeDetails.java
+++ app/src/main/java/com/example/justcook/activities/RecipeDetails.java
@@ -9,7 +9,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.justcook.R;
 import com.example.justcook.model.Comment;
@@ -73,4 +75,13 @@ public class RecipeDetails extends AppCompatActivity {
         layout.setLayoutParams(paramsText);
     }
 
+    public void sendComment(View view) {
+        //TODO: TUTAJ NALEŻY WSTAWIĆ LOGIKĘ WSTAWIANIA KOMENTARZA DO BAZY DANYCH
+        EditText editText = findViewById(R.id.comment_EditText_details);
+        String comment = editText.getText().toString(); //TODO: GOTOWA ZAWARTOŚĆ KOMENTARZA
+        editText.setText("");
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+        Toast toast = Toast.makeText(getApplicationContext(), "Your comment is being sent", Toast.LENGTH_SHORT);
+        toast.show();
+    }
 }
