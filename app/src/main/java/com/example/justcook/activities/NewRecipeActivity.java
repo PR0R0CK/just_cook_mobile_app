8df8eb12b2dacc651d94d3459453a0270a51fe88
Lukasz Zaranek <l.zaranek01@gmail.com>
2020-06-06 23:29:20 +0200

    Szkielet dodawania nowego przepisu, bez logiki, go chyba spanko, ale nie wiem jeszcze

--- app/src/main/java/com/example/justcook/activities/NewRecipeActivity.java
+++ app/src/main/java/com/example/justcook/activities/NewRecipeActivity.java
@@ -3,6 +3,9 @@ package com.example.justcook.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.justcook.R;
 
@@ -12,5 +15,18 @@ public class NewRecipeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_recipe);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+        //Ustawienie śmigła poziomu trudności
+        Spinner spinner = findViewById(R.id.difficulty_spinner_new);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.difficulty, R.layout.spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        Spinner spinnerType = findViewById(R.id.type_spinner_new);
+        adapter = ArrayAdapter.createFromResource(this, R.array.types, R.layout.spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerType.setAdapter(adapter);
+
+
     }
 }
